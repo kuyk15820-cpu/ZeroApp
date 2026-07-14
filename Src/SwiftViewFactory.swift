86cb9e70d84fffa48de82e14878843c7de8aafad
kuyk15820-cpu <kuyk15820@gmail.com>
2026-07-15 00:39:06 +0700

    Update SwiftViewFactory.swift

--- Src/SwiftViewFactory.swift
+++ Src/SwiftViewFactory.swift
@@ -1,10 +1,11 @@
 import SwiftUI
+import UIKit
 
 @objc class SwiftViewFactory: NSObject {
-    @objc static func createMainView() -> UIViewController {
-        let mainView = MainView()
+    @objc static func createMainView(onSelectVideo: @escaping () -> Void) -> UIViewController {
+        let mainView = MainView(onSelectVideo: onSelectVideo)
         let hostingController = UIHostingController(rootView: mainView)
-        hostingController.view.backgroundColor = .black
+        hostingController.view.backgroundColor = UIColor.black
         return hostingController
     }
 }
