892b49b22f2fdf7f658bb7ac867413ef8c986c37
kuyk15820-cpu <kuyk15820@gmail.com>
2026-07-15 00:53:28 +0700

    Update SwiftViewFactory.swift

--- Src/SwiftViewFactory.swift
+++ Src/SwiftViewFactory.swift
@@ -1,8 +1,12 @@
 import SwiftUI
 import UIKit
 
-@objc class SwiftViewFactory: NSObject {
-    @objc static func createMainView(onSelectVideo: @escaping () -> Void) -> UIViewController {
+@objc(SwiftViewFactory) // บังคับชื่อคลาสใน Objective-C Runtime
+class SwiftViewFactory: NSObject {
+    
+    // บังคับชื่อ Selector ให้ตรงกับที่ RootViewController.mm เรียกใช้เป๊ะๆ
+    @objc(createMainViewWithOnSelectVideo:)
+    static func createMainView(onSelectVideo: @escaping () -> Void) -> UIViewController {
         let mainView = MainView(onSelectVideo: onSelectVideo)
         let hostingController = UIHostingController(rootView: mainView)
         hostingController.view.backgroundColor = UIColor.black
